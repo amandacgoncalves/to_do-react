f8dd77437a2233b6c34bd374d283e2c4e091f744
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-06 09:01:27 -0300

    component pageheader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import pageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <pageHeader name="Sobre" small="Nós"></pageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
     </div>
 )
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import pageHeader from '../template/pageHeader'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <pageHeader name="Tarefas" small="Cadastro"></pageHeader>
             </div>
         )
     }
