774b49b6fef9aecce7440d75ad7f2a93b1052ede
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-06 08:35:34 -0300

    component to do nd about

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,10 +1,13 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 import React from 'React'
 
 export default props => (
     <div className='container'>
-        <h1>Teste</h1>
+        <Todo />
+        <About />
     </div>
 )
